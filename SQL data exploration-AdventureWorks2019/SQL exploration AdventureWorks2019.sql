c441552ce540174fb15e07e2594bce9e915e4438
Ahamed Azam <96620728+azam-analyst@users.noreply.github.com>
2022-03-29 22:57:30 +0100

    Update SQL exploration AdventureWorks2019.sql

--- SQL data exploration-AdventureWorks2019/SQL exploration AdventureWorks2019.sql
+++ SQL data exploration-AdventureWorks2019/SQL exploration AdventureWorks2019.sql
@@ -4,7 +4,7 @@
 ----WELCOME to this SQL practice queries on aggregate functions,JOINS,Sub Queries,CTE and Window Function--------
 
   ( This is a project on data exploration with SQL on Microsoft SQL Server Management studio on AdventureWorks2019 dataset containing 68 tables.This practice project 
-  has been done to enhance my SQL skill as well as to showcase my data analysis ability with SQL especially with Joins,Aggregate functions and sub queries.
+  has been done to enhance my SQL skill as well as to showcase my data analysis ability with SQL especially with Joins,Aggregate functions and sub queries.)
 
 *****************************************************************************************************************/
 
